ea02cd1dc713195ecc8a68b2855da7b1cb7783af
jump6746 <jump6746@naver.com>
2025-07-09 17:02:53 +0900

    fix: 빌드 에러 수정

--- src/views/Landing/ui/LandingPage.tsx
+++ src/views/Landing/ui/LandingPage.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from "next/link";
 import GuestButton from "@/views/Landing/ui/GuestButton";
 import OAuthLoginButton from "@/widgets/auth/ui/OAuthLoginButton";
